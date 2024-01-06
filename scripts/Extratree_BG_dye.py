081a3c808a17b55fd94e2516e93fbc46d45784ed
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-06 20:22:15 +0530

    Create Extratree_BG_dye.py

---
+++ scripts/Extratree_BG_dye.py
@@ -0,0 +1,54 @@
+data = pd.read_excel("BG.xlsx")
+data_subset = data.head(2000)
+
+X = data_subset[['Stirringspeed', 'Temp', 'Time', 'Dosage', 'pH', 'Concentration']]
+y = data_subset[['Concentration,Cf(mg/L)', 'Adsorption capacity(mg/g)', 'Adsorption efficiency(%)']]
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+extra_trees_regressor = ExtraTreesRegressor(random_state=42)
+
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['auto', 'sqrt', 'log2']
+}
+
+grid_search = GridSearchCV(extra_trees_regressor, param_grid, cv=5, scoring='neg_mean_squared_error')
+grid_search.fit(X_train, y_train)
+
+best_params = grid_search.best_params_
+print("Best Hyperparameters:", best_params)
+
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
+# Calculate mean squared error (MSE) and R-squared (R2)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Squared Error (MSE):", mse)
+print("R-squared (R2):", r2)
+df = pd.read_excel('predicted_data_with_inputs_extrareg_BG.xlsx')
+df_subset = df.head(200)
+df.head(200)
+df_subset = df.head(200)
+sns.set_theme(style='darkgrid')
+
+#Subplots
+sns.regplot(x='Concentration,Cf(mg/L)', y ='Predicted_Concentration,Cf(mg/L)', data = df_subset,
+           scatter_kws={"s": 60, "color": 'snow', "marker": 'o', "edgecolor": 'blue'},
+            line_kws={"color": 'indigo', "linestyle": '--'})
+plt.tight_layout()
+plt.show()
+sns.regplot(x='Adsorption capacity(mg/g)', y ='Predicted_Adsorption capacity(mg/g)', data = df_subset,
+            scatter_kws={"s": 60, "color": 'snow', "marker": 'o', "edgecolor": 'blue'},
+            line_kws={"color": 'indigo', "linestyle": '--'})
+plt.tight_layout()
+plt.show()
+sns.regplot(x='Adsorption efficiency(%)', y ='Predicted_Adsorption efficiency(%)', data = df_subset,
+            scatter_kws={"s": 60, "color": 'snow', "marker": 'o', "edgecolor": 'blue'},
+          line_kws={"color": 'indigo', "linestyle": '--'})
+plt.tight_layout()
+plt.show(

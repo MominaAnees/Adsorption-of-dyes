9a9138aa80ef8ddd585cc95611f6fcb672831178
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-06 18:55:49 +0530

    Update Pearson correlation.py

--- scripts/Pearson correlation.py
+++ scripts/Pearson correlation.py
@@ -1,28 +1,36 @@
-"""
-==================
-1. Pearson correlation
-==================
-"""
-print("\033[14;1mMR DYE\033[m")
-import pandas as pd
-data = pd.read_excel("Data.xlsx", skiprows=1)
-data.head()
+.. code-block:: python
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-data = pd.read_excel("Data.xlsx", skiprows=1)
-correlation_matrix = data.corr()
+    """
+    ==================
+    1. Pearson correlation
+    ==================
+    """
+    print("\033[14;1mMR DYE\033[m")
+    import pandas as pd
+    data = pd.read_excel("Data.xlsx", skiprows=1)
+    data.head()
 
-plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='BrBG', fmt=".2f")
-plt.title("Correlation Heatmap")
-plt.show()
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    data = pd.read_excel("Data.xlsx", skiprows=1)
+    correlation_matrix = data.corr()
 
-print("\033[14;1mBG DYE\033[m")
-data = pd.read_excel("BG.xlsx")
-print(data.describe())
-correlation_matrix = data.corr()
-plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='BrBG', fmt=".2f")
-plt.title("Correlation Heatmap for BG dye")
-plt.show()
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap='BrBG', fmt=".2f")
+    plt.title("Correlation Heatmap")
+    plt.show()
+
+    print("\033[14;1mBG DYE\033[m")
+    data = pd.read_excel("BG.xlsx")
+    print(data.describe())
+    correlation_matrix = data.corr()
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap='BrBG', fmt=".2f")
+    plt.title("Correlation Heatmap for BG dye")
+    plt.show()
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Scripts
+
+   auto_examples/Pearson_correlation

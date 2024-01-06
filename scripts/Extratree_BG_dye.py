7924ca1516a2c6361847a82c30681f35119ca0ae
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-06 20:24:59 +0530

    Update Extratree_BG_dye.py

--- scripts/Extratree_BG_dye.py
+++ scripts/Extratree_BG_dye.py
@@ -1,3 +1,18 @@
+"""
+==================
+6. Extra tree for BG dye
+==================
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
 data = pd.read_excel("BG.xlsx")
 data_subset = data.head(2000)
 

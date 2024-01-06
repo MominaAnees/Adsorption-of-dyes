7e16cf3470867c0e862f1e699689765220e48ada
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-06 18:29:35 +0530

    Update Pearson correlation.py

--- scripts/Pearson correlation.py
+++ scripts/Pearson correlation.py
@@ -7,6 +7,7 @@ print("\033[14;1mMR DYE\033[m")
 import pandas as pd
 data = pd.read_excel("Data.xlsx", skiprows=1)
 data.head()
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 data = pd.read_excel("Data.xlsx", skiprows=1)
@@ -16,6 +17,7 @@ plt.figure(figsize=(10, 8))
 sns.heatmap(correlation_matrix, annot=True, cmap='BrBG', fmt=".2f")
 plt.title("Correlation Heatmap")
 plt.show()
+
 print("\033[14;1mBG DYE\033[m")
 data = pd.read_excel("BG.xlsx")
 print(data.describe())

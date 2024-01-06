4cf0d8e30895bb4a3667b93ac108e5c9e208c50e
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-06 16:50:17 +0530

    Create Pearson correlation.py

---
+++ scripts/Pearson correlation.py
@@ -0,0 +1,26 @@
+"""
+==================
+2. Pearson correlation
+==================
+"""
+print("\033[14;1mMR DYE\033[m")
+import pandas as pd
+data = pd.read_excel("Data.xlsx", skiprows=1)
+data.head()
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_excel("Data.xlsx", skiprows=1)
+correlation_matrix = data.corr()
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='BrBG', fmt=".2f")
+plt.title("Correlation Heatmap")
+plt.show()
+print("\033[14;1mBG DYE\033[m")
+data = pd.read_excel("BG.xlsx")
+print(data.describe())
+correlation_matrix = data.corr()
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='BrBG', fmt=".2f")
+plt.title("Correlation Heatmap for BG dye")
+plt.show()

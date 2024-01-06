8e110bd1fb34af02ebc12d5bad01b9c02534b72f
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-06 19:23:34 +0530

    Update RF_MR_dye.py

--- scripts/RF_MR_dye.py
+++ scripts/RF_MR_dye.py
@@ -1,30 +1,27 @@
-    """
-    ==================
-    4. RF for MR dye
-    ==================
-    """
+"""
+==================
+4. RF for MR dye
+==================
+"""
 
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.model_selection import train_test_split
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.metrics import mean_squared_error, r2_score
-    import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 
 data = pd.read_excel("Data.xlsx", skiprows=1)
-
 
 # Split data into inputs (X) and outputs (y)
 X = data.drop(columns=['Stirringspeed', 'Temp', 'Time', 'Dosage', 'pH', 'Concentration'], axis=1)
 y = data[['Concentration,Cf(mg/L)', 'Adsorption capacity(mg/g)', 'Adsorption efficiency(%)']]
 
-
 X.columns = X.columns.astype(str)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
-model = RandomForestRegressor(n_estimators=200, max_features='sqrt',bootstrap=True, max_depth=None, oob_score=True, random_state=42)
-
+model = RandomForestRegressor(n_estimators=200, max_features='sqrt', bootstrap=True, max_depth=None, oob_score=True, random_state=42)
 
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -39,18 +36,18 @@ df.head()
 df_subset = df.head(200)
 sns.set_theme(style='darkgrid')
 # Subplots
-sns.regplot(x='Concentration,Cf(mg/L)', y ='Predicted_Concentration,Cf(mg/L)', data = df_subset,
-           scatter_kws={"s": 60, "color": 'snow', "marker": 'o', "edgecolor": 'blue'},
-            line_kws={"color": 'indigo', "linestyle": '--'})
-plt.tight_layout()
-plt.show()
-sns.regplot(x='Adsorption capacity(mg/g)', y ='Predicted_Adsorption capacity(mg/g)', data = df_subset,
+sns.regplot(x='Concentration,Cf(mg/L)', y='Predicted_Concentration,Cf(mg/L)', data=df_subset,
             scatter_kws={"s": 60, "color": 'snow', "marker": 'o', "edgecolor": 'blue'},
             line_kws={"color": 'indigo', "linestyle": '--'})
 plt.tight_layout()
 plt.show()
-sns.regplot(x='Adsorption efficiency(%)', y ='Predicted_Adsorption efficiency(%)', data = df_subset,
+sns.regplot(x='Adsorption capacity(mg/g)', y='Predicted_Adsorption capacity(mg/g)', data=df_subset,
             scatter_kws={"s": 60, "color": 'snow', "marker": 'o', "edgecolor": 'blue'},
-          line_kws={"color": 'indigo', "linestyle": '--'})
+            line_kws={"color": 'indigo', "linestyle": '--'})
+plt.tight_layout()
+plt.show()
+sns.regplot(x='Adsorption efficiency(%)', y='Predicted_Adsorption efficiency(%)', data=df_subset,
+            scatter_kws={"s": 60, "color": 'snow', "marker": 'o', "edgecolor": 'blue'},
+            line_kws={"color": 'indigo', "linestyle": '--'})
 plt.tight_layout()
 plt.show()

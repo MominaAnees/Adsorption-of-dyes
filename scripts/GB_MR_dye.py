b724311f6575bda85e2a024b9a078a163cbdf701
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-08 11:22:10 +0530

    Update GB_MR_dye.py

--- scripts/GB_MR_dye.py
+++ scripts/GB_MR_dye.py
@@ -5,11 +5,11 @@
 """
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.multioutput import MultiOutputRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 data = pd.read_excel("Data.xlsx", skiprows=1)
 data_subset = data.head(2000)
@@ -21,7 +21,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 gb_regressor = GradientBoostingRegressor(n_estimators=100, max_depth=3, random_state=42)
 gb_regressor.fit(X_train, y_train.values.ravel())  # Note: ravel() to convert y_train to 1D array
 
-gb_regressor.fit(X_train, y_train)
 y_pred = gb_regressor.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 print("Mean Squared Error:", mse)
@@ -29,7 +28,6 @@ r2 = r2_score(y_test, y_pred)
 print("R2 Score:", r2)
 
 test_score = np.zeros((gb_regressor.n_estimators,))
-
 
 for i, y_pred in enumerate(gb_regressor.staged_predict(X_test)):
     test_score[i] = mean_squared_error(y_test, y_pred)
@@ -52,4 +50,3 @@ plt.xlabel("n_estimator")
 plt.ylabel("Deviance")
 fig.tight_layout()
 plt.show()
-

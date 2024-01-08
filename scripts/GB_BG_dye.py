2b5d2648559db69fb0ac8824661bc416a119f1d8
MominaAnees <144117347+MominaAnees@users.noreply.github.com>
2024-01-08 11:19:53 +0530

    Update GB_BG_dye.py

--- scripts/GB_BG_dye.py
+++ scripts/GB_BG_dye.py
@@ -5,21 +5,26 @@
 """
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.multioutput import MultiOutputRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+
 data = pd.read_excel("BG.xlsx")
 data_subset = data.head(2000)
 X = data_subset.drop(columns=['Stirringspeed', 'Temp', 'Time', 'Dosage', 'pH', 'Concentration'], axis=1)
 y = data_subset[['Concentration,Cf(mg/L)', 'Adsorption capacity(mg/g)', 'Adsorption efficiency(%)']]
 
-
+# Use test_size as a fraction of the dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
 gb_regressor = GradientBoostingRegressor(n_estimators=100, max_depth=3, random_state=42)
+
+# Fit the model
 gb_regressor.fit(X_train, y_train.values.ravel())  
 
+# Calculate the test set deviance
 test_score = np.zeros((gb_regressor.n_estimators,))
 for i, y_pred in enumerate(gb_regressor.staged_predict(X_test)):
     test_score[i] = mean_squared_error(y_test, y_pred)
@@ -42,11 +47,13 @@ plt.xlabel("n_estimator")
 plt.ylabel("Deviance")
 fig.tight_layout()
 plt.show()
+
+# Re-fit the model on the entire training set
 gb_regressor.fit(X_train, y_train)
 
-
+# Make predictions on the test set
 y_pred = gb_regressor.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
-print("Mean Squared Error:", mse)
 r2 = r2_score(y_test, y_pred)
+print("Mean Squared Error:", mse)
 print("R2 Score:", r2)
